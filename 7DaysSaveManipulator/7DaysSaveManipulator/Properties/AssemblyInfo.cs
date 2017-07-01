e3c5bdba029aba035c48d4c565693edf6226e3a2
Karlovsky120 <karlovsky120@gmail.com>
2017-07-01 16:06:49 +0200

    Refactored project, it's now a standalone solution.

--- 7DaysSaveManipulator/7DaysSaveManipulator/Properties/AssemblyInfo.cs
+++ 7DaysSaveManipulator/7DaysSaveManipulator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("7DaysSaveManipulator")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © Karlovsky120")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
